d581ea76e0c38aa4d9b3a7f58460ad58db358b1d
Shah Musharaf ul Islam <90516265+Musharraf1128@users.noreply.github.com>
2025-04-20 17:17:40 +0530

    Update vite.config.js for vercel now

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Tenzies/',
   plugins: [react()],
-  server: {
-    headers: {
-      'Permissions-Policy': 'interest-cohort=()',
-    },
-  },
 })
